78cdff864d27ed24472dc4eb68792f8e9ceb41be
MrDanielHarka <79216552+MrDanielHarka@users.noreply.github.com>
2021-05-05 16:43:25 +0200

    Added files

--- practice/js-essential-training-linkedin/Practice/03_07/script.js
+++ practice/js-essential-training-linkedin/Practice/03_07/script.js
@@ -7,3 +7,14 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const myPhone = {
+    name: "Samsung",
+    model: "A5",
+    size: 5.2,
+    buttons: 4,
+    isOn: true,
+    turnOnOff: function (onStatus) {
+        this.isOn = onStatus;
+    }
+}
