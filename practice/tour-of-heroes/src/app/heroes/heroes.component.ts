14409b36fab5d199d65a8831c0cc49dfa3682e1b
MrDanielHarka <79216552+MrDanielHarka@users.noreply.github.com>
2021-05-16 17:04:16 +0200

    Added files

---
+++ practice/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { HEROES } from "./mock-heroes";
+
+@Component ({
+    selector: 'app-heroes',
+    templateUrl: 'heroes.component.html',
+    styleUrls: ['heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+    
+    heroes = HEROES;
+    
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
+
---
+++ practice/tour-of-heroes/src/app/heroes/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 2, name: 'Superman'},
+    {id: 3, name: 'X-man'},
+    {id: 4, name: 'Batwoman'},
+    {id: 5, name: 'Aquaman'},
+    {id: 6, name: 'Pacman'}
+];
